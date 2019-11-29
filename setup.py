5d749495dca205a4dcabfa2f625d96b7a6b2b5c6
chrstphr <chrstphr@posteo.eu>
2019-11-29 14:57:20 +0100

    version-less dependencies for now

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ import setuptools
 VERSION = '0.1'
 
 def run_setup():
-    requires = [
-        'numpy>=1.15.3',
-        'Pillow>=5.3.0',
-        'six>=1.11.0',
-        'torch>=0.4.1.post2',
-        'torchvision>=0.2.1',
-    ]
     setup(
         name="zennit",
         version=VERSION,
         packages=find_packages(),
-        install_requires=requires)
+        install_requires=[
+            'numpy',
+            'Pillow',
+            'torch',
+            'torchvision',
+        ],
+    )
 
 run_setup()
