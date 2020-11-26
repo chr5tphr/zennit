9b1973bfbd45a14aba1cfb40c337642528a29b1e
chrstphr <chrstphr@posteo.eu>
2020-11-26 17:55:58 +0100

    added activation and avg pool types

--- zennit/types.py
+++ zennit/types.py
@@ -41,3 +41,44 @@ class BatchNorm(metaclass=SubclassMeta):
         torch.nn.modules.batchnorm.BatchNorm2d,
         torch.nn.modules.batchnorm.BatchNorm3d,
     )
+
+
+class AvgPool(metaclass=SubclassMeta):
+    '''Abstract base class that describes sum-pooling modules.'''
+    __subclass__ = (
+        torch.nn.modules.pooling.AvgPool1d,
+        torch.nn.modules.pooling.AvgPool2d,
+        torch.nn.modules.pooling.AvgPool3d,
+        torch.nn.modules.pooling.AdaptiveAvgPool1d,
+        torch.nn.modules.pooling.AdaptiveAvgPool2d,
+        torch.nn.modules.pooling.AdaptiveAvgPool3d,
+    )
+
+
+class Activation(metaclass=SubclassMeta):
+    '''Abstract base class that describes activation modules.'''
+    __subclass__ = (
+        torch.nn.modules.activation.ELU,
+        torch.nn.modules.activation.Hardshrink,
+        torch.nn.modules.activation.Hardsigmoid,
+        torch.nn.modules.activation.Hardtanh,
+        torch.nn.modules.activation.Hardswish,
+        torch.nn.modules.activation.LeakyReLU,
+        torch.nn.modules.activation.LogSigmoid,
+        torch.nn.modules.activation.MultiheadAttention,
+        torch.nn.modules.activation.PReLU,
+        torch.nn.modules.activation.ReLU,
+        torch.nn.modules.activation.ReLU6,
+        torch.nn.modules.activation.RReLU,
+        torch.nn.modules.activation.SELU,
+        torch.nn.modules.activation.CELU,
+        torch.nn.modules.activation.GELU,
+        torch.nn.modules.activation.Sigmoid,
+        torch.nn.modules.activation.SiLU,
+        torch.nn.modules.activation.Softplus,
+        torch.nn.modules.activation.Softshrink,
+        torch.nn.modules.activation.Softsign,
+        torch.nn.modules.activation.Tanh,
+        torch.nn.modules.activation.Tanhshrink,
+        torch.nn.modules.activation.Threshold,
+    )
