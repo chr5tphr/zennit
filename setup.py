2e36f2b45d4babf3aaaeb00c7e7e9e853e0f3fc3
chrstphr <chrstphr@posteo.eu>
2021-03-26 19:08:50 +0100

    use find_packages in setup.py to find submodules
    also allow higher versions of pytorch

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="zennit",
     use_scm_version=True,
-    packages=['zennit'],
+    packages=find_packages(include=['zennit*']),
     install_requires=[
         'numpy',
         'Pillow',
-        'torch==1.7.0',
+        'torch>=1.7.0',
     ],
     setup_requires=[
         'setuptools_scm',
