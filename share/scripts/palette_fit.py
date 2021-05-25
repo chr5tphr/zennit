123b89c9067e5d2580a86b71579e64fb34b9655e
chrstphr <chrstphr@posteo.eu>
2021-05-25 17:02:27 +0200

    Use float for dist computations in palette_fit

--- share/scripts/palette_fit.py
+++ share/scripts/palette_fit.py
@@ -36,19 +36,20 @@ def gale_shapley(dist):
 @click.option('--strategy', type=click.Choice(['intensity', 'nearest', 'histogram']), default='intensity')
 @click.option('--source-cmap', type=click.Choice(list(CMAPS)), default='bwr')
 @click.option('--source-level', type=float, default=1.0)
+@click.option('--invert/--no-invert', default=False)
 @click.option('--cmap', type=click.Choice(list(CMAPS)), default='coldnhot')
 @click.option('--level', type=float, default=1.0)
-def main(source_file, output_file, strategy, source_cmap, source_level, cmap, level):
+def main(source_file, output_file, strategy, source_cmap, source_level, invert, cmap, level):
     '''Fit an existing RGB heatmap image to a color palette.'''
     source = np.array(Image.open(source_file).convert('RGB'))
     matchpal = palette(source_cmap, source_level)
 
     if strategy == 'intensity':
         # matching based on the source image intensity/ brigthness
-        values = 255 - np.array(source).sum(2) // 3
+        values = source.astype(float).mean(2)
     elif strategy == 'nearest':
         # match by finding the neareast centroids in a source colormap
-        dists = (np.abs(source[None] - matchpal[:, None, None])).sum(3)
+        dists = (np.abs(source[None].astype(float) - matchpal[:, None, None].astype(float))).sum(3)
         values = np.argmin(dists, axis=0)
     elif strategy == 'histogram':
         # match by finding a stable match between the color histogram of the source image and a source colormap
@@ -62,7 +63,11 @@ def main(source_file, output_file, strategy, source_cmap, source_level, cmap, le
         values = np.zeros(source.shape[:2], dtype=np.uint8)
         values[ind_h, ind_w] = matches[ind_bin]
 
-    img = Image.fromarray(values.astype(np.uint8), mode='P')
+    values = values.clip(0, 255).astype(np.uint8)
+    if invert:
+        values = 255 - values
+
+    img = Image.fromarray(values, mode='P')
     pal = palette(cmap, level)
     img.putpalette(pal)
     img.save(output_file)
