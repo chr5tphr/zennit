c12013ea085a7b8fdb61d77ffa4b4005850c8e09
chrstphr <chrstphr@posteo.eu>
2022-01-17 18:51:52 +0100

    Style fixes for tests/test_canonizers.py
    - missing docstring for merge_batchnorm_consistency
    - assertion list comprehension changed to generator

--- tests/test_canonizers.py
+++ tests/test_canonizers.py
@@ -5,6 +5,7 @@ from zennit.canonizers import SequentialMergeBatchNorm
 
 
 def test_merge_batchnorm_consistency(module_linear, module_batchnorm, data_input):
+    '''Test whether the output of the merged batchnorm is consistent with its original output.'''
     output_linear_before = module_linear(data_input)
     output_batchnorm_before = module_batchnorm(output_linear_before)
     canonizer = SequentialMergeBatchNorm()
@@ -19,9 +20,9 @@ def test_merge_batchnorm_consistency(module_linear, module_batchnorm, data_input
     output_linear_after = module_linear(data_input)
     output_batchnorm_after = module_batchnorm(output_linear_after)
 
-    assert all([torch.allclose(left, right, atol=1e-5) for left, right in [
+    assert all(torch.allclose(left, right, atol=1e-5) for left, right in [
         (output_linear_before, output_linear_after),
         (output_batchnorm_before, output_batchnorm_after),
         (output_batchnorm_before, output_linear_canonizer),
         (output_linear_canonizer, output_batchnorm_canonizer),
-    ]])
+    ])
