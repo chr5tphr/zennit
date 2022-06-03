cca9ff525b149ceae368039553a4498dc4f0f9ed
chrstphr <chrstphr@posteo.eu>
2022-06-03 17:02:14 +0200

    Package: Import all modules in zennit/__init__.py
    - to enable top-level import of zennit
    - I did not initially do this as I did not feel this was necessary; now
      I feel differently

---
+++ src/zennit/__init__.py
@@ -0,0 +1,25 @@
+'''Zennit top-level __init__.'''
+from . import attribution
+from . import canonizers
+from . import cmap
+from . import composites
+from . import core
+from . import image
+from . import layer
+from . import rules
+from . import torchvision
+from . import types
+
+
+__all__ = [
+    'attribution',
+    'canonizers',
+    'cmap',
+    'composites',
+    'core',
+    'image',
+    'layer',
+    'rules',
+    'torchvision',
+    'types',
+]
