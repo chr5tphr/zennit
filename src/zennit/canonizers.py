661d280f3fcaef85208e8342a469a2f7c786286a
jackmcrider <756997+jackmcrider@users.noreply.github.com>
2023-08-24 10:29:04 +0200

    Update src/zennit/canonizers.py
    change `setattr(parent_module, ...)` to `parent_module.add_module(...)`
    
    Co-authored-by: Christopher <15217558+chr5tphr@users.noreply.github.com>

--- src/zennit/canonizers.py
+++ src/zennit/canonizers.py
@@ -418,9 +418,10 @@ class KMeansCanonizer(Canonizer):
         weight = weight[mask].reshape(n_clusters, n_clusters - 1, n_dims)
         norms = torch.norm(self.distance.centroids, dim=-1)
         bias = (norms[None, :]**2 - norms[:, None]**2)[mask].reshape(n_clusters, n_clusters - 1)
-        setattr(self.parent_module, self.child_name,
-                torch.nn.Sequential(NeuralizedKMeans(weight, bias),
-                                    LogMeanExpPool(self.beta)))
+        self.parent_module.add_module(
+            self.child_name,
+            torch.nn.Sequential(NeuralizedKMeans(weight, bias), LogMeanExpPool(self.beta))
+        )
 
     def remove(self):
         """Revert the changes introduced by this canonizer."""
